bff633d8313b5d9c584bd6c8eeae5462683ea317
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-09-16 16:56:07 -0300

    Adding collection view delegate and data source

---
+++ MacroPepelelipa/MacroPepelelipa/Controller/Workspace/CollectionView/CollectionViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewDelegate.swift
+//  MacroPepelelipa
+//
+//  Created by Pedro Giuliano Farina on 16/09/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+internal class WorkspaceCollectionViewDelegate: NSObject, UICollectionViewDelegate {
+
+}
---
+++ MacroPepelelipa/MacroPepelelipa/Model/Workspace/CollectionViewDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  WorkspaceCollectionViewDataSource.swift
+//  MacroPepelelipa
+//
+//  Created by Pedro Giuliano Farina on 16/09/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+internal class WorkspaceCollectionViewDataSource: NSObject, UICollectionViewDataSource {
+
+}
