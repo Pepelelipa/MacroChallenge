028902a3f50d77e8fb87f050d87890f18f14f8cb
liakassardjian <liakassardjian@gmail.com>
2020-09-17 14:36:52 -0300

    Created custom TextViewDelegate to the markup view

--- MacroPepelelipa/MacroPepelelipa/View/TextViewDelegate.swift
+++ MacroPepelelipa/MacroPepelelipa/View/TextViewDelegate.swift
@@ -8,6 +8,33 @@
 
 import UIKit
 
-class TextViewDelegate: UITextViewDelegate {
+class TextViewDelegate: NSObject, UITextViewDelegate {
 
+    var renderer: MarkupRenderer
+    var text: String
+
+    init(renderer: MarkupRenderer) {
+        self.renderer = renderer
+        self.text = ""
+    }
+
+    func render(on textView: UITextView) {
+        textView.attributedText = renderer.render(text: self.text)
+    }
+
+    func textViewDidChange(_ textView: UITextView) {
+        render(on: textView)
+    }
+
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let char = text.cString(using: String.Encoding.utf8)!
+        let isBackSpace = strcmp(char, "\\b")
+        if isBackSpace == -92 {
+            self.text.removeLast()
+        } else {
+            self.text.append(text)
+        }
+        return true
+    }
+    
 }
