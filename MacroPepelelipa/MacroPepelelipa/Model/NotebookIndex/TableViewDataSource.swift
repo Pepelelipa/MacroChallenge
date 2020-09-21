92c6459baac09f3430ab5cf0c1b12e02ae5d22d2
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-09-21 14:55:20 -0300

    Removing FatalError() and trailing spaces

--- MacroPepelelipa/MacroPepelelipa/Model/NotebookIndex/TableViewDataSource.swift
+++ MacroPepelelipa/MacroPepelelipa/Model/NotebookIndex/TableViewDataSource.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 internal class NotebookIndexTableViewDataSource: NSObject, UITableViewDataSource {
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let cell = tableView.dequeueReusableCell(
                 withIdentifier: NotebookIndexTableViewCell.cellID, for: indexPath)
             as? NotebookIndexTableViewCell else {
-                fatalError("Error with reusable table-view cell object for the specified reuse identifier")
+            return UITableViewCell()
         }
 
         return cell
--- MacroPepelelipa/MacroPepelelipa/View/NotebookIndex/NotebookIndexTableViewCell.swift
+++ MacroPepelelipa/MacroPepelelipa/View/NotebookIndex/NotebookIndexTableViewCell.swift
@@ -10,10 +10,10 @@ import UIKit
 
 #warning("Notebook TableView Cell has no actual information yet.")
 internal class NotebookIndexTableViewCell: UITableViewCell {
-    
+
     internal static let cellID = "notebookIndexCell"
     private let lessonLbl: UILabel = UILabel(frame: .zero)
-    
+
     internal var lessonTitle: String? {
         get {
             return lessonLbl.text
@@ -22,13 +22,13 @@ internal class NotebookIndexTableViewCell: UITableViewCell {
             lessonLbl.text = newValue
         }
     }
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .random()
         setupLabel()
     }
-    
+
     required convenience init?(coder: NSCoder) {
         guard let style = coder.decodeObject(forKey: "style") as? UITableViewCell.CellStyle,
               let reuseIdentifier = coder.decodeObject(forKey: "reuseIdentifier") as? String else {
@@ -36,12 +36,12 @@ internal class NotebookIndexTableViewCell: UITableViewCell {
         }
         self.init(style: style, reuseIdentifier: reuseIdentifier)
     }
-    
+
     private func setupLabel() {
         lessonTitle = "Lesson".localized()
         lessonLbl.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(lessonLbl)
-        
+
         NSLayoutConstraint.activate([
             lessonLbl.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
             lessonLbl.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
