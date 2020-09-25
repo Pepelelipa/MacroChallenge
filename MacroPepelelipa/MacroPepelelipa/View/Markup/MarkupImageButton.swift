b48811e3351243c4ccb2b69d356c8d8f870ba851
Paula Leite <paula.t.leite@icloud.com>
2020-09-25 15:45:34 -0300

    The configuration of the button is finished

---
+++ MacroPepelelipa/MacroPepelelipa/View/Markup/MarkupImageButton.swift
@@ -0,0 +1,9 @@
+//
+//  MarkupImageButton.swift
+//  MacroPepelelipa
+//
+//  Created by Paula Leite on 25/09/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+import Foundation
