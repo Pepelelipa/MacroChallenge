5e5f45c529f1d4507c458d7b607acfd6c71fb05f
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-09-30 15:42:59 -0300

    Adding book and rearraging groups

--- MacroPepelelipa/MacroPepelelipa/View/Workspace/WorkspaceCellNotebookCollectionViewCell.swift
+++ MacroPepelelipa/MacroPepelelipa/View/Workspace/WorkspaceCellNotebookCollectionViewCell.swift
@@ -10,4 +10,45 @@ import UIKit
 
 internal class WorkspaceCellNotebookCollectionViewCell: UICollectionViewCell {
     internal static let cellID = "workspaceCellNotebookCell"
+
+    private let shadowView = UIImageView(image: #imageLiteral(resourceName: "BooklikeShadow"))
+    private let bookView = UIImageView(image: #imageLiteral(resourceName: "Book"))
+    public var color: UIColor {
+        get {
+            return bookView.tintColor
+        }
+        set {
+            bookView.tintColor = newValue
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bookView.addSubview(shadowView)
+        addSubview(bookView)
+        setupConstraints()
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            shadowView.centerXAnchor.constraint(equalTo: bookView.centerXAnchor),
+            shadowView.centerYAnchor.constraint(equalTo: bookView.centerYAnchor),
+            shadowView.widthAnchor.constraint(equalTo: bookView.widthAnchor),
+            shadowView.heightAnchor.constraint(equalTo: bookView.heightAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            bookView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            bookView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            bookView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            bookView.heightAnchor.constraint(equalTo: contentView.heightAnchor)
+        ])
+    }
+
+    required convenience init?(coder: NSCoder) {
+        guard let frame = coder.decodeObject(forKey: "frame") as? CGRect else {
+            return nil
+        }
+        self.init(frame: frame)
+    }
 }
