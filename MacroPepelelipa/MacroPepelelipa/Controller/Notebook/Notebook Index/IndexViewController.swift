8e6c357640ae066ab316ec72c1af44c908fb0aab
Leonardo Amorim de Oliveira <leonardo.aoliveira@icloud.com>
2020-10-02 17:25:34 -0300

    Feat/displaying notebook notes (#27)
    * Displaying note for each cell at NotebookIndexndex
    
    * Updated index mock image to notebook image with correct color
    
    * Set detail at SplitViewController as private

--- MacroPepelelipa/MacroPepelelipa/Controller/Notebook/Notebook Index/IndexViewController.swift
+++ MacroPepelelipa/MacroPepelelipa/Controller/Notebook/Notebook Index/IndexViewController.swift
@@ -13,10 +13,8 @@ internal class NotebookIndexViewController: UIViewController {
     internal private(set) var notebook: NotebookEntity?
     internal init(notebook: NotebookEntity) {
         self.notebook = notebook
-
-        imgViewNotebook.tintColor = UIColor(named: notebook.colorName)
         lblSubject.text = notebook.name
-        dataSource = NotebookIndexTableViewDataSource(notebook: notebook)
+        tableViewDataSource = NotebookIndexTableViewDataSource(notebook: notebook)
         
         super.init(nibName: nil, bundle: nil)
     }
@@ -48,9 +46,11 @@ internal class NotebookIndexViewController: UIViewController {
         
         return btn
     }()
-    private let imgViewNotebook: UIImageView = {
-        let imgView = UIImageView(frame: .zero)
-        imgView.image = UIImage(systemName: "book")
+    private lazy var imgViewNotebook: NotebookView = {
+        let imgView = NotebookView(frame: .zero)
+        if let color = UIColor(named: self.notebook?.colorName ?? "") {
+            imgView.color = color
+        }
         imgView.translatesAutoresizingMaskIntoConstraints = false
         imgView.contentMode = .scaleAspectFill
 
@@ -65,10 +65,18 @@ internal class NotebookIndexViewController: UIViewController {
 
         return lbl
     }()
-    private let dataSource: NotebookIndexTableViewDataSource
+    private let tableViewDataSource: NotebookIndexTableViewDataSource
+    private lazy var tableViewDelegate: NotebookIndexTableViewDelegate = NotebookIndexTableViewDelegate { [unowned self] (selectedCell) in
+        guard let note = selectedCell.indexNote else {
+            fatalError("The index did not have a note")
+        }
+        
+        self.splitViewController?.showDetailViewController(NotesViewController(note: note), sender: self)
+    }
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero)
-        tableView.dataSource = dataSource
+        tableView.dataSource = tableViewDataSource
+        tableView.delegate = tableViewDelegate
         tableView.tableFooterView = UIView()
 
         tableView.backgroundColor = view.backgroundColor
