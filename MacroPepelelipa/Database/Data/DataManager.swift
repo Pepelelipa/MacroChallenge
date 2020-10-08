3c1167d073825671a8d55dababa4204d119a8302
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-10-08 18:04:33 -0300

    Adding documentation to DataManager

--- MacroPepelelipa/Database/Data/DataManager.swift
+++ MacroPepelelipa/Database/Data/DataManager.swift
@@ -10,12 +10,22 @@ public class DataManager {
     private let coreDataController = CoreDataController()
 
     //MARK: Workspace
+    /**
+     Creates a Workspace into the Database
+     - Parameter name: The workspace's  name.
+     - Throws: Throws if fails to create in CoreData.
+     */
     public func createWorkspace(named name: String) throws -> WorkspaceEntity {
         let cdWorkspace = try coreDataController.createWorkspace(named: name)
 
         return WorkspaceObject(from: cdWorkspace)
     }
 
+    /**
+     Deletes a workspace from the Database
+     - Parameter workspace: Workspace to be deleted.
+     - Throws: Throws if fails to parse object to WorkspaceObject or fails to delete from CoreData.
+     */
     public func deleteWorkspace(_ workspace: WorkspaceEntity) throws {
         guard let workspaceObject = workspace as? WorkspaceObject else {
             throw WorkspaceError.FailedToParse
@@ -25,6 +35,13 @@ public class DataManager {
     }
 
     //MARK: Notebook
+    /**
+     Creates a Notebook into the Database
+     - Parameter workspace: To what workspace it belongs.
+     - Parameter name: The notebook's name.
+     - Parameter colorName: The nootebook's color name.
+     - Throws: Throws if fails to parse workspace to WorkspaceObject or fails to create in CoreData.
+     */
     public func createNotebook(in workspace: WorkspaceEntity, named name: String, colorName: String) throws -> NotebookEntity {
         guard let workspaceObject = workspace as? WorkspaceObject else {
             throw WorkspaceError.FailedToParse
@@ -34,6 +51,11 @@ public class DataManager {
         return NotebookObject(in: workspaceObject, from: cdNotebook)
     }
 
+    /**
+     Deletes a notebook from Database
+     - Parameter notebook: Notebook to be deleted.
+     - Throws: Throws if fails to parse object to NotebookObject or fails to delete from CoreData.
+     */
     public func deleteNotebook(_ notebook: NotebookEntity) throws {
         guard let notebookObject = notebook as? NotebookObject else {
             throw NotebookError.FailedToParse
@@ -43,6 +65,11 @@ public class DataManager {
     }
 
     //MARK: Note
+    /**
+     Creates a Note into the Database
+     - Parameter notebook: To what notebook it belongs.
+     - Throws: Throws if fails to parse notebook to NotebookObject or fails to create in CoreData.
+     */
     public func createNote(in notebook: NotebookEntity) throws -> NoteEntity {
         guard let notebookObject = notebook as? NotebookObject else {
             throw NotebookError.FailedToParse
@@ -52,6 +79,11 @@ public class DataManager {
         return NoteObject(in: notebookObject, from: cdNote)
     }
 
+    /**
+     Deletes a note from Database
+     - Parameter note: Note to be deleted.
+     - Throws: Throws if fails to parse object to NoteObject or fails to delete from CoreData.
+     */
     public func deleteNote(_ note: NoteEntity) throws {
         guard let noteObject = note as? NoteObject else {
             throw NoteError.FailedToParse
@@ -61,6 +93,11 @@ public class DataManager {
     }
 
     //MARK: TextBox
+    /**
+     Creates a TextBox into the Database
+     - Parameter note: To what note it belongs.
+     - Throws: Throws if fails to parse note to NoteObject or fails to create in CoreData.
+     */
     public func createTextBox(in note: NoteEntity) throws -> TextBoxEntity {
         guard let noteObject = note as? NoteObject else {
             throw NoteError.FailedToParse
@@ -70,6 +107,11 @@ public class DataManager {
         return TextBoxObject(in: noteObject, coreDataObject: cdTextBox)
     }
 
+    /**
+     Deletes a TextBox from Database
+     - Parameter textBox: TextBox to be deleted.
+     - Throws: Throws fails to parse object to TextBoxObject or fails to delete from CoreData.
+     */
     public func deleteTextBox(_ textBox: TextBoxEntity) throws {
         guard let textBoxObject = textBox as? TextBoxObject else {
             throw TextBoxError.FailedToParse
@@ -79,6 +121,11 @@ public class DataManager {
     }
 
     //MARK: ImageBox
+    /**
+     Creates a ImageBox into the Database
+     - Parameter note: To what note it belongs.
+     - Throws: Throws if fails to parse note to NoteObject or fails to create in CoreData.
+     */
     public func createImageBox(in note: NoteEntity) throws -> ImageBoxEntity {
         guard let noteObject = note as? NoteObject else {
             throw NoteError.FailedToParse
@@ -88,6 +135,11 @@ public class DataManager {
         return ImageBoxObject(in: noteObject, coreDataObject: cdImageBox)
     }
 
+    /**
+     Deletes a ImageBox from Database
+     - Parameter imageBox: ImageBox to be deleted.
+     - Throws: Throws fails to parse object to ImageBoxObject or fails to delete from CoreData.
+     */
     public func deleteImageBox(_ imageBox: ImageBoxEntity) throws {
         guard let imageBoxObject = imageBox as? ImageBoxObject else {
             throw ImageBoxError.FailedToParse
