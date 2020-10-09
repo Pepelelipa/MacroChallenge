01239e6cee1160f233351b0a1a2baec0569b84b4
Paula Leite <paula.t.leite97@gmail.com>
2020-10-09 17:43:59 -0300

    Now the buttons are correct and the popover is coming out of the correct place

--- MacroPepelelipa/MacroPepelelipa/View/Markup/MarkupNavigationView.swift
+++ MacroPepelelipa/MacroPepelelipa/View/Markup/MarkupNavigationView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-internal class MarkupNavigationView: UIStackView {
+internal class MarkupNavigationView: UIView {
     private weak var markupBarConfiguration: MarkupBarConfiguration?
     
     private static var paragraphButton: UIBarButtonItem?
@@ -30,15 +30,22 @@ internal class MarkupNavigationView: UIStackView {
         return buttons
     }()
     
+    private let stackView = UIStackView()
+    
     init(frame: CGRect, configurations: MarkupBarConfiguration) {
         self.markupBarConfiguration = configurations
         super.init(frame: frame)
         
-        setButtonConstraints()
+        barButtonItems.forEach({
+            self.addSubview($0)
+        })
+        
+        self.addSubview(stackView)
+        
+        setStackView()
         
         self.sizeToFit()
         self.tintColor = UIColor.toolsColor
-
     }
     
     required init(coder: NSCoder) {
@@ -46,22 +53,46 @@ internal class MarkupNavigationView: UIStackView {
     }
     
     /**
-     A private method to set up all the buttons on the navigation bar.
+     A private method to set the buttons inside of a stack view.
      */
-    private func setButtonConstraints() {
-        guard let barButtonItems = markupBarConfiguration?.setupUIButtons() else {
-            return
-        }
+    private func setStackView() {
         
-        self.axis = .horizontal
-        self.alignment = .fill
-        self.distribution = .fillProportionally
-        self.spacing = 5.0
+//        stackView.axis = .horizontal
+//        stackView.alignment = .fill
+//        stackView.distribution = .fillProportionally
+//        stackView.spacing = 5.0
+//
+//        stackView.addArrangedSubview(barButtonItems[3])
+//        stackView.addArrangedSubview(barButtonItems[2])
+//        stackView.addArrangedSubview(barButtonItems[0])
+//        stackView.addArrangedSubview(barButtonItems[4])
+//        stackView.addArrangedSubview(barButtonItems[1])
         
-        self.addArrangedSubview(barButtonItems[3])
-        self.addArrangedSubview(barButtonItems[2])
-        self.addArrangedSubview(barButtonItems[0])
-        self.addArrangedSubview(barButtonItems[4])
-        self.addArrangedSubview(barButtonItems[1])
+//        setConstraints()
+        barButtonItems.forEach({
+            NSLayoutConstraint.activate([
+                $0.topAnchor.constraint(equalTo: self.topAnchor),
+                $0.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+                $0.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.8),
+                $0.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.9)
+            ])
+        })
+        
+        NSLayoutConstraint.activate([
+            barButtonItems[3].leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            barButtonItems[2].leadingAnchor.constraint(equalTo: barButtonItems[3].trailingAnchor, constant: 10),
+            barButtonItems[4].leadingAnchor.constraint(equalTo: barButtonItems[2].trailingAnchor, constant: 10),
+            barButtonItems[0].leadingAnchor.constraint(equalTo: barButtonItems[4].trailingAnchor, constant: 10),
+            barButtonItems[1].leadingAnchor.constraint(equalTo: barButtonItems[0].trailingAnchor, constant: 10)
+        ])
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
     }
 }
