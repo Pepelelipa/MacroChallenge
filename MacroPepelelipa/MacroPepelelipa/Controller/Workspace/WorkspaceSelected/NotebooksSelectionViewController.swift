011851d72716496eb04b60850012bfa029d06bc9
liakassardjian <liakassardjian@gmail.com>
2020-10-14 14:35:52 -0300

    Added deletion feature to notebooks

--- MacroPepelelipa/MacroPepelelipa/Controller/Workspace/WorkspaceSelected/NotebooksSelectionViewController.swift
+++ MacroPepelelipa/MacroPepelelipa/Controller/Workspace/WorkspaceSelected/NotebooksSelectionViewController.swift
@@ -22,24 +22,24 @@ internal class NotebooksSelectionViewController: UIViewController {
         }
         self.init(workspace: workspace)
     }
-
+    
     private lazy var collectionView: UICollectionView = {
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-
+        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = view.backgroundColor
         collectionView.showsVerticalScrollIndicator = false
         collectionView.allowsSelection = true
         collectionView.allowsMultipleSelection = false
-
+        
         collectionView.delegate = collectionDelegate
         collectionView.dataSource = collectionDataSource
-
+        
         collectionView.register(
             NotebookCollectionViewCell.self,
             forCellWithReuseIdentifier: NotebookCollectionViewCell.cellID())
-
+        
         return collectionView
     }()
     private var collectionDataSource: NotebooksCollectionViewDataSource?
@@ -55,7 +55,7 @@ internal class NotebooksSelectionViewController: UIViewController {
             return
         }
         let split = SplitViewController(notebook: notebook)
-
+        
         #warning("Fade animation as placeholder for Books animation.")
         let transition = CATransition()
         transition.duration = 0.4
@@ -65,7 +65,7 @@ internal class NotebooksSelectionViewController: UIViewController {
         
         self.present(split, animated: false)
     }
-
+    
     private lazy var btnAdd: UIBarButtonItem = {
         let item = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(btnAddTap))
         return item
@@ -81,27 +81,30 @@ internal class NotebooksSelectionViewController: UIViewController {
         })
         addController.moveTo(self)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.rightBarButtonItem = btnAdd
         navigationItem.title = workspace?.name
         view.backgroundColor = .backgroundColor
         view.addSubview(collectionView)
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gesture:)))
+        self.collectionView.addGestureRecognizer(longPressGesture)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             self.collectionView.collectionViewLayout.invalidateLayout()
         }
     }
-
+    
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         collectionView.collectionViewLayout.invalidateLayout()
     }
-
+    
     override func viewDidLayoutSubviews() {
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
@@ -110,5 +113,30 @@ internal class NotebooksSelectionViewController: UIViewController {
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    @IBAction func handleLongPress(gesture: UILongPressGestureRecognizer) {
+        let point = gesture.location(in: collectionView)
+        
+        guard let indexPath = collectionView.indexPathForItem(at: point),
+              let cell = collectionView.cellForItem(at: indexPath) as? NotebookCollectionViewCell,
+              let notebook = cell.notebook else {
+            return
+        }
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet).makeDeleteConfirmation(dataType: .notebook, deletionHandler: { _ in
+            do {
+                _ = try DataManager.shared().deleteNotebook(notebook)
+            } catch {
+                let alertController = UIAlertController(
+                    title: "Could not delete this notebook".localized(),
+                    message: "The app could not delete the notebook".localized() + notebook.name,
+                    preferredStyle: .alert)
+                    .makeErrorMessage(with: "An error occurred while deleting this instance on the database".localized())
+                self.present(alertController, animated: true, completion: nil)
+            }
+        })
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 }
