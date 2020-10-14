91a07b186ebb22c6da8a5528eb415d96a88e3698
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-10-14 19:55:02 -0300

    Refactoring container view

--- MacroPepelelipa/MacroPepelelipa/Controller/Notebook/TextEditingContainerViewController.swift
+++ MacroPepelelipa/MacroPepelelipa/Controller/Notebook/TextEditingContainerViewController.swift
@@ -5,23 +5,17 @@
 //  Created by Leonardo Oliveira on 14/10/20.
 //  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
 //
+//swiftlint:disable multiple_closures_with_trailing_closure
 
 import UIKit
 import Database
-
-internal enum SlideOutState {
-    case collapsed
-    case rightPanelExpanded
-}
 
 internal class TextEditingContainerViewController: UIViewController {
     
     // MARK: - Variables and Constants
     
     private var centerViewController: NotesViewController?
-    private var rightViewController: NotebookIndexViewController?
-    private var currentState: SlideOutState = .collapsed
-    private lazy var centerPanelExpandedOffset: CGFloat = view.frame.width * 0.6
+    private weak var rightViewController: NotebookIndexViewController?
     
     private lazy var notebookIndexButton: UIBarButtonItem = {
         let item = UIBarButtonItem(image: UIImage(systemName: "n.square"), 
@@ -50,92 +44,73 @@ internal class TextEditingContainerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let centerViewController = self.centerViewController, 
-           let notebook = centerViewController.notebook {
-            
-            rightViewController = NotebookIndexViewController(notebook: notebook)
+        if let centerViewController = self.centerViewController {
             view.addSubview(centerViewController.view)
             addChild(centerViewController)
-        
         } else {
             navigationController?.popViewController(animated: true)
         }
         
         navigationItem.rightBarButtonItem = notebookIndexButton
-    }
-    
-    override func viewWillLayoutSubviews() {
-        
-        if currentState == .rightPanelExpanded,
-           let rightView = self.rightViewController?.view,
-           let centerView = self.centerViewController?.view {
-            
-            rightView.translatesAutoresizingMaskIntoConstraints = false
-            
-            rightView.removeConstraints([
-            ])
-            
-            rightView.removeConstraints(rightView.constraints)
-            centerPanelExpandedOffset = view.frame.width * 0.6
-            
-            centerView.frame.origin.x = -view.frame.width + centerPanelExpandedOffset
-            
-            NSLayoutConstraint.activate([
-                rightView.topAnchor.constraint(equalTo: view.topAnchor),
-                rightView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                rightView.widthAnchor.constraint(equalToConstant: view.frame.width * 0.4),
-                rightView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            ])
-        }
-    }
-    
-    // MARK: - Functions
-    
-    func addChildSidePanelController(_ sidePanelController: NotebookIndexViewController) {
-        view.insertSubview(sidePanelController.view, at: 0)
-        addChild(sidePanelController)
-        sidePanelController.didMove(toParent: self)
-        
-        animateRightPanel(shouldExpand: true)
-    }
-    
-    func animateRightPanel(shouldExpand: Bool) {
-        if shouldExpand {
-            animateCenterPanelXPosition(
-                targetPosition: -view.frame.width + centerPanelExpandedOffset)
-        } else {
-            animateCenterPanelXPosition(targetPosition: 0) { _ in
-                self.rightViewController?.view.removeFromSuperview()
-            }
-        }
-        viewDidLayoutSubviews()
-    }
-    
-    func animateCenterPanelXPosition(targetPosition: CGFloat, completion: ((Bool) -> Void)? = nil) {
-      UIView.animate(withDuration: 0.5,
-                     delay: 0,
-                     usingSpringWithDamping: 1,
-                     initialSpringVelocity: 0,
-                     options: .curveEaseInOut, 
-                     animations: {
-                        self.centerViewController?.view.frame.origin.x = targetPosition
-                        
-      }, completion: completion)
+        navigationItem.largeTitleDisplayMode = .never
+        view.backgroundColor = .rootColor
     }
     
     // MARK: - IBActions functions
-    
+    var movement: CGFloat?
+    var widthConstraint: NSLayoutConstraint?
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        widthConstraint?.isActive = true
+    }
+
     @IBAction private func presentNotebookIndex() {
-        
-        let notAlreadyExpanded = (currentState != .rightPanelExpanded)
-        
-        if notAlreadyExpanded,
-           let notebookIndexViewController = self.rightViewController {
-            currentState = .rightPanelExpanded
-            addChildSidePanelController(notebookIndexViewController)
+        if let rightViewController = rightViewController {
+            //Hide
+            UIView.animate(withDuration: 0.5) {
+                for child in self.view.subviews {
+                    child.frame.origin.x += self.movement ?? rightViewController.view.frame.width
+                }
+            } completion: { _ in
+                rightViewController.willMove(toParent: nil)
+                rightViewController.removeFromParent()
+                rightViewController.view.removeFromSuperview()
+                rightViewController.didMove(toParent: nil)
+            }
+
+            self.rightViewController = nil
+        } else if let notebook = centerViewController?.notebook {
+            //Show
+            let rightViewController = NotebookIndexViewController(notebook: notebook)
+            rightViewController.willMove(toParent: self)
+            addChild(rightViewController)
+            let rightView: UIView = rightViewController.view
+            rightView.translatesAutoresizingMaskIntoConstraints = false
+
+            view.addSubview(rightView)
+            rightView.frame = CGRect(x: view.frame.maxX, y: view.frame.maxY, width: 0, height: view.frame.height)
+
+            let widthConstraint = rightView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4)
+            self.widthConstraint = widthConstraint
+
+            NSLayoutConstraint.activate([
+                widthConstraint,
+                rightView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                rightView.topAnchor.constraint(equalTo: view.topAnchor),
+                rightView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+            rightViewController.didMove(toParent: self)
+            self.movement = view.frame.width * 0.4
+
+            UIView.animate(withDuration: 0.5) {
+                for child in self.view.subviews {
+                    child.frame.origin.x -= self.view.frame.width * 0.4
+                }
+            }
+
+            self.rightViewController = rightViewController
         } else {
-            currentState = .collapsed
-            animateRightPanel(shouldExpand: notAlreadyExpanded)
+            fatalError("Ops")
         }
     }
     
