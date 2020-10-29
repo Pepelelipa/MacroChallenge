9cba95be657cc602ee6c8846d6897ddb5454ddf8
pedrohgsilveira <pedrohgsilveira@gmail.com>
2020-10-29 16:42:08 -0300

    Add documentation

--- MacroPepelelipa/MacroPepelelipa/Model/Workspace/FilterWorkspaceObserver.swift
+++ MacroPepelelipa/MacroPepelelipa/Model/Workspace/FilterWorkspaceObserver.swift
@@ -10,6 +10,13 @@ import Foundation
 import Database
 
 internal protocol FilterObserver: class {
+    
+    /**
+    This method communicates the Data Source, from the UICollectionView, which text is inserted in the search bar and the type of the search category.
+     - Parameter searchText: The text entered in the search bar.
+     - Parameter filterCategory: The type of object to be fetched.
+     */
     func filterObjects(_ searchText: String, filterCategory: SearchResultEnum?)
+    
     func isFiltering(_ value: Bool)
 }
