b633b5496c536fb97871db5379dc5bac1b22fc34
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-11-03 16:30:24 -0300

    Adding markdown framework

---
+++ MacroPepelelipa/MarkdownText/MarkdownText.h
@@ -0,0 +1,19 @@
+//
+//  MarkdownText.h
+//  MarkdownText
+//
+//  Created by Pedro Giuliano Farina on 03/11/20.
+//  Copyright © 2020 Pedro Giuliano Farina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarkdownText.
+FOUNDATION_EXPORT double MarkdownTextVersionNumber;
+
+//! Project version string for MarkdownText.
+FOUNDATION_EXPORT const unsigned char MarkdownTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkdownText/PublicHeader.h>
+
+
