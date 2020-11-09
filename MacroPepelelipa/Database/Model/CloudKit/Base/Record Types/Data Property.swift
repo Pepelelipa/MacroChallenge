14b7842fb3cd8cf1e88cddbc048bbb9a965214d0
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-11-09 14:51:09 -0300

    Adding Reference Field

--- MacroPepelelipa/Database/Model/CloudKit/Base/Record Types/Data Property.swift
+++ MacroPepelelipa/Database/Model/CloudKit/Base/Record Types/Data Property.swift
@@ -8,10 +8,10 @@
 
 import CloudKit
 
-public class DataProperty<T: Equatable> {
+internal class DataProperty<T: Equatable> {
     private let record: CKRecord
     private let key: String
-    public var value: T {
+    internal var value: T {
         get {
             guard let val = record.value(forKey: key) as? T else {
                 fatalError("Couldn't convert data to associated type.")
