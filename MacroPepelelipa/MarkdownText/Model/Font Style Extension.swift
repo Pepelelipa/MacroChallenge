961be3c32d15026879a3b113491fa9d2e7eb800b
pedrohgsilveira <49411884+pedrohgsilveira@users.noreply.github.com>
2020-11-09 16:56:14 -0300

    Feat/tip shortcut (#124)
    * Adding tip view controller
    
    * Implementing Tip View Controller
    
    * Shortcut implemented
    
    * Merge with dev
    
    * Error Handling
    
    * Pull Request Fixes
    
    * Fixing bold and italic
    
    Co-authored-by: Pedro Giuliano Farina <pedrogfarina@gmail.com>

--- MacroPepelelipa/MarkdownText/Model/Font Style Extension.swift
+++ MacroPepelelipa/MarkdownText/Model/Font Style Extension.swift
@@ -53,4 +53,64 @@ public extension UIFont {
             return self.withSize(18).bold() ?? .boldSystemFont(ofSize: 18)
         }
     }
+    
+    func bold() -> UIFont? {
+        return withTraits(.traitBold)
+    }
+    
+    func italic() -> UIFont? {
+        return withTraits(.traitItalic)
+    }
+
+    func light() -> UIFont? {
+        return UIFont.systemFont(ofSize: UIFont.systemFontSize, weight: .thin)
+    }
+    
+    // MARK: - Trait Functions
+
+    /**
+     This method adds traits to a UIFont.
+     - Parameter traits: The Symbolic Traits to describe the font.
+     - Returns: An optional UIFont with the described traits.
+     */
+    func withTraits(_ traits: UIFontDescriptor.SymbolicTraits...) -> UIFont? {
+        var newTraits = fontDescriptor.symbolicTraits
+        for trait in traits {
+            if !newTraits.contains(trait) {
+                newTraits.insert(trait)
+            }
+        }
+
+        guard let descriptor = fontDescriptor.withSymbolicTraits(newTraits) else {
+            return nil
+        }
+        return UIFont(descriptor: descriptor, size: 0)
+    }
+
+    ///Checks if a font has some trait
+    func hasTrait(_ trait: UIFontDescriptor.SymbolicTraits) -> Bool {
+        return self.fontDescriptor.symbolicTraits.contains(trait)
+    }
+
+    /**
+     This method removes a trait from a UIFont.
+     - Parameters:
+     - font: The UIFont that will be checked to remove the trait.
+     - trait: A symbolic trait describing the trait to be removed.
+
+     - Returns: A UIFont without the chosen trait.
+     */
+    func removeTrait(_ trait: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        var traits = self.fontDescriptor.symbolicTraits
+
+        if traits.contains(trait) {
+            traits.remove(trait)
+        }
+
+        if let descriptor = self.fontDescriptor.withSymbolicTraits(traits) {
+            return UIFont(descriptor: descriptor, size: 0)
+        }
+
+        return self
+    }
 }
