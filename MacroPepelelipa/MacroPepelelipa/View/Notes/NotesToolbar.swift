adbd83d8508f33124ba388940e6c61b12e427247
LeoAOliveira <leonardo.aoliveira@icloud.com>
2020-11-12 17:47:23 -0300

    Setting NotesToolbar ites according to device

--- MacroPepelelipa/MacroPepelelipa/View/Notes/NotesToolbar.swift
+++ MacroPepelelipa/MacroPepelelipa/View/Notes/NotesToolbar.swift
@@ -98,7 +98,11 @@ internal class NotesToolbar: UIToolbar {
         
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         
-        self.items = [deleteNoteButton, flexibleSpace, addImageButton, flexibleSpace, shareNoteButton, flexibleSpace, newNoteButton]
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            self.items = [deleteNoteButton, flexibleSpace, addImageButton, flexibleSpace, shareNoteButton, flexibleSpace, newNoteButton]
+        } else {
+            self.items = [deleteNoteButton, flexibleSpace, shareNoteButton, flexibleSpace, newNoteButton]
+        }
     }
     
     // MARK: - IBActions Functions
