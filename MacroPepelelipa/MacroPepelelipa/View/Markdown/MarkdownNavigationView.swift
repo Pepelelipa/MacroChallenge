6ce830cb2a2a275189fef2f22c4e103fac2d8bfa
paulaleite <48655864+paulaleite@users.noreply.github.com>
2020-11-30 17:44:59 -0300

    Correction to the white color problem (#175)

--- MacroPepelelipa/MacroPepelelipa/View/Markdown/MarkdownNavigationView.swift
+++ MacroPepelelipa/MacroPepelelipa/View/Markdown/MarkdownNavigationView.swift
@@ -12,7 +12,6 @@ internal class MarkdownNavigationView: UIView {
     
     // MARK: - Variables and Constants
     
-    private var navigationItem: UINavigationItem?
     private weak var markupBarConfiguration: MarkdownBarConfiguration?
     private static var paragraphButton: UIBarButtonItem?
     
