a01acd1c1b5550157f86d6db4975d5170d41cb2b
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-11-30 18:31:13 -0300

    Removing all strong references from observers

--- MacroPepelelipa/MacroPepelelipa/Controller/Notebook/Notes/MarkdownTextViewDelegate.swift
+++ MacroPepelelipa/MacroPepelelipa/Controller/Notebook/Notes/MarkdownTextViewDelegate.swift
@@ -13,28 +13,34 @@ internal protocol MarkdownObserver: class {
     func didChangeSelection(_ textView: UITextView)
 }
 
+fileprivate struct MarkdownObserverReference {
+    weak var value: MarkdownObserver?
+}
+
 internal class AppMarkdownTextViewDelegate: MarkdownTextViewDelegate {
     
-    private var markdownObservers: [MarkdownObserver] = []
+    private var markdownObservers: [MarkdownObserverReference] = []
     
     internal func addMarkdownObserver(_ observer: MarkdownObserver) {
-        markdownObservers.append(observer)
+        let reference = MarkdownObserverReference(value: observer)
+        markdownObservers.append(reference)
     }
     
     internal func removeMarkdownObserver(_ observer: MarkdownObserver) {
-        if let index = markdownObservers.firstIndex(where: { $0 === observer }) {
+        if let index = markdownObservers.firstIndex(where: { $0.value === observer }) {
             markdownObservers.remove(at: index)
         }
     }
 
-    private var textObservers: [TextEditingDelegateObserver] = []
+    private var textObservers: [TextEditingDelegateObserverReference] = []
     
     internal func addTextObserver(_ observer: TextEditingDelegateObserver) {
-        textObservers.append(observer)
+        let reference = TextEditingDelegateObserverReference(value: observer)
+        textObservers.append(reference)
     }
     
     internal func removeTextObserver(_ observer: TextEditingDelegateObserver) {
-        if let index = textObservers.firstIndex(where: { $0 === observer }) {
+        if let index = textObservers.firstIndex(where: { $0.value === observer }) {
             textObservers.remove(at: index)
         }
     }
@@ -42,21 +48,24 @@ internal class AppMarkdownTextViewDelegate: MarkdownTextViewDelegate {
     override func textViewDidChangeSelection(_ textView: UITextView) {
         super.textViewDidChangeSelection(textView)
         for observer in markdownObservers {
-            observer.didChangeSelection(textView)
+            observer.value?.didChangeSelection(textView)
         }
+        markdownObservers.removeAll(where: { $0.value == nil })
     }
 
     override func textViewDidBeginEditing(_ textView: UITextView) {
         super.textViewDidBeginEditing(textView)
         for observer in textObservers {
-            observer.textEditingDidBegin()
+            observer.value?.textEditingDidBegin()
         }
+        textObservers.removeAll(where: { $0.value == nil })
     }
 
     override func textViewDidEndEditing(_ textView: UITextView) {
         super.textViewDidEndEditing(textView)
         for observer in textObservers {
-            observer.textEditingDidEnd()
+            observer.value?.textEditingDidEnd()
         }
+        textObservers.removeAll(where: { $0.value == nil })
     }
 }
