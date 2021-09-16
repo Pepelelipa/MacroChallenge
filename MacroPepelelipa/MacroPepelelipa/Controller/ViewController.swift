43379ee97b59a106ebea064c4356f66ad2c0b88e
liakassardjian <liakassardjian@gmail.com>
2021-09-16 19:28:51 -0300

    Created default ViewController with shortcuts observers

---
+++ MacroPepelelipa/MacroPepelelipa/Controller/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  MacroPepelelipa
+//
+//  Created by Lia Kassardjian on 16/09/21.
+//  Copyright © 2021 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+internal protocol KeyboardShortcutDelegate: AnyObject {
+    func commandN()
+    func commandF()
+    func commandB()
+    func commandI()
+    func commandU()
+    func commandDelete()
+    func commandShiftN()
+}
+
+internal class ViewController: UIViewController {
+
+    internal weak var keyboardShortcutDelegate: KeyboardShortcutDelegate?
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addObservers()
+    }
+    
+    private func addObservers() {
+        for name in Notification.Name.keyboardShortcuts {
+            NotificationCenter.default.addObserver(self, selector: #selector(notificationHandler(_:)), name: name, object: nil)
+        }
+    }
+
+    @objc private func notificationHandler(_ key: String) {
+        let notification = Notification.Name(key)
+        
+        switch notification {
+        case .didPressCommandN:
+            keyboardShortcutDelegate?.commandN()
+            
+        case .didPressCommandF:
+            keyboardShortcutDelegate?.commandF()
+
+        case .didPressCommandB:
+            keyboardShortcutDelegate?.commandB()
+
+        case .didPressCommandI:
+            keyboardShortcutDelegate?.commandI()
+
+        case .didPressCommandU:
+            keyboardShortcutDelegate?.commandU()
+
+        case .didPressCommandDelete:
+            keyboardShortcutDelegate?.commandDelete()
+
+        case .didPressCommandShiftN:
+            keyboardShortcutDelegate?.commandShiftN()
+            
+        default:
+            break
+        }
+    }
+}
